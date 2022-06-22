d9a95bd01fe7ee94088f5ba863aa995a4c740b48
Rominger B <robuan.31@gmail.com>
2022-06-21 20:50:31 -0500

    cambio de puerto

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ server = app.server
 
 # Testing server, don't use in production, host
 if __name__ == "__main__":
-    app.run_server(host='0.0.0.0', port=8050, debug=True)
+    app.run_server(host='0.0.0.0', port=8080, debug=True)
