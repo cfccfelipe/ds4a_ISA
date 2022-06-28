3565694b88af0554ef5ac3ffc934ba3355d469cd
Nataly Mariño <nmarino@LAPTOP-9JM75IQ1.localdomain>
2022-06-27 21:24:04 -0500

    actualizar uso de dash core y dash html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # libraries
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 import pandas as pd
 import plotly.graph_objs as obj
 import uvicorn as uvicorn
